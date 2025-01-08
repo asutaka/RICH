6de17d5b029c14ca98ffe7ea40815aee975bdd20
Nguyễn Văn Phú <asutaka@outlook.com>
2025-01-08 15:07:08 +0700

    First commit

---
+++ Coin/Program.cs
@@ -0,0 +1,10 @@
+using Coin;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
