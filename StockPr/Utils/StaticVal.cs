5d7aa968ca27a6cff3896e7fb3ea2a08320d95e7
Nguyễn Văn Phú <asutaka@outlook.com>
2025-02-01 23:07:49 +0700

    update token

--- StockPr/Utils/StaticVal.cs
+++ StockPr/Utils/StaticVal.cs
@@ -9,8 +9,8 @@ namespace StockPr.Utils
         public static int _TAKE = 15;
         public static int _curQuarter = 20244;
         public static (long, long, long, long) _currentTime;//yearquarter + year + quarter + flag
-        public static string _VietStock_Cookie = "ASP.NET_SessionId=nlehv103ci5raqevopu2cysl; __RequestVerificationToken=Mmg6CGomYCXe4BZkIkoJRLvUmhZ99CUb4coeUeaymZCN9xYHumQFNeWQBeu2TlJriagiaA-3vecniiRfEVRWQBIsYA-Gi-PysFMLQ8FjpXY1; language=vi-VN; _ga=GA1.1.1634064005.1724032252; _ga_EXMM0DKVEX=GS1.1.1724032251.1.0.1724032251.60.0.0; AnonymousNotification=; Theme=Light; vts_usr_lg=8E7FC9B57A7F485E1BF6E003F8700B04A197873D535172F8B72CBF3E943E96E892AE7919BFE55F31E327735F1C715D801A1EEA9F5DEE728F9642A841504B610E1D6B04D958309DAEDE842A620B055DDD0480800FD63C2D047B2CB31F794DC93F9B7E7116E87AFEDB3CF0C90934AC0B47950BC5585E235CFF35E59885BEFCDCCE";
-        public static string _VietStock_Token = "wVIyNRGpnFhrFclsY80ON85OurU8C1z0U53Yhn8uPuHtKkP2RNX7XMWZQXaP3xTANcAMaFUCAcCkgD5lAxbLRJ6t89Ui-MFsrh90SL6z57ygdjrSXm9sxaLvFZCYx0im0";
+        public static string _VietStock_Cookie = "ASP.NET_SessionId=arn5rig5dxbmmjoizukepdtp; __RequestVerificationToken=lsueRAFEInzD1hnxKUdgW2KwoJG9BTcIbmeErlT-ZbsnP-s9bR_xm25X1mXPVAj19x9vzoY6uLnwZW889qjTsK-YscWe_4RZQo5ErQDjJBE1";
+        public static string _VietStock_Token = "7JGS5cE1JOnO0ahmaHORE-TfLL2cwOSn8PW2HOlAi9hrGZcTIx1bHmNKAh5cCTNjz6Aaz3BLDHOQwdZ6JAqACPuAC_TfSi3tVisBTv_JkYF6juHbiKElDlPSkt5lrDzZ0";
 
         public static List<DateTime> _lNghiLe = new List<DateTime>
         {
