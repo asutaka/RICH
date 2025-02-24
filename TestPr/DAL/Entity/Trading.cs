99fe76f43a6c11febdde1c2f3a1f435f20293a8e
Nguyễn Văn Phú <asutaka@outlook.com>
2025-02-24 22:32:12 +0700

    update

--- TestPr/DAL/Entity/Trading.cs
+++ TestPr/DAL/Entity/Trading.cs
@@ -5,6 +5,7 @@ namespace TestPr.DAL.Entity
     [BsonIgnoreExtraElements]
     public class Trading : BaseDTO
     {
+        public string key { get; set; }
         public string s { get; set; }
         public int d { get; set; }//Date
         public int Side { get; set; }//Buy Or Sell
@@ -13,11 +14,21 @@ namespace TestPr.DAL.Entity
         public double TP { get; set; }//Only OrderBlock
         public double Focus { get; set; }
         public int Status { get; set; }//Only Liquid
-
-        public double AvgPrice { get; set; }
-        public double PriceAtLiquid { get; set; }
-        public double CurrentPrice { get; set; }
-        public int Mode { get; set; }
         public DateTime Date { get; set; }
+        //For Test
+        public double CurrentPrice { get; set; }
+        public double AvgPrice { get; set; }
+        public double Liquid { get; set; }
+        public double Rsi { get; set; }//5p
+        public double TopFirst { get; set; }
+        public double TopNext { get; set; }
+        public double BotFirst { get; set; }
+        public double BotNext { get; set; }
+        public double RateVol { get; set; }
+        public double TP_2 { get; set; }
+        public double TP_3 { get; set; }
+        public double TP25 { get; set; }
+        public double SL_2 { get; set; }
+        public double SL25 { get; set; }
     }
 }
