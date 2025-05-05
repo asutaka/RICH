11fcc51e475f95e1e2d076c675d8d59b720a7217
Nguyễn Văn Phú <phunv@ftech.ai>
2025-05-05 09:58:37 +0700

    clean

--- TradePr/DAL/Settings/RegisterDAL.cs
+++ TradePr/DAL/Settings/RegisterDAL.cs
@@ -7,7 +7,6 @@
             services.AddSingleton<ITradingRepo, TradingRepo>();
             services.AddSingleton<IConfigDataRepo, ConfigDataRepo>();
             services.AddSingleton<ISymbolRepo, SymbolRepo>();
-            services.AddSingleton<ISymbolConfigRepo, SymbolConfigRepo>();
             services.AddSingleton<IPlaceOrderTradeRepo, PlaceOrderTradeRepo>();
         }
     }
