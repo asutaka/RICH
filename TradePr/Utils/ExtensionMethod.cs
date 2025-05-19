3fc550417369ec8e0666f07db623be283b3aabd6
Nguyễn Văn Phú <asutaka@outlook.com>
2025-05-19 20:40:34 +0700

    zz

--- TradePr/Utils/ExtensionMethod.cs
+++ TradePr/Utils/ExtensionMethod.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Skender.Stock.Indicators;
+using System.ComponentModel.DataAnnotations;
 using System.Reflection;
 
 namespace TradePr.Utils
@@ -33,6 +34,94 @@ namespace TradePr.Utils
             {
                 return string.Empty;
             }
+        }
+
+        public static bool IsExistTopB(this List<Quote> lData)
+        {
+            try
+            {
+                var lbb = lData.GetBollingerBands();
+                var lCheck = lData.TakeLast(20);
+                var count = lCheck.Count();
+                for (int i = 4; i < count - 3; i++)
+                {
+                    var prev_1 = lCheck.ElementAt(i - 4);
+                    var prev_2 = lCheck.ElementAt(i - 3);
+                    var prev_3 = lCheck.ElementAt(i - 2);
+                    var prev_4 = lCheck.ElementAt(i - 1);
+                    var cur = lCheck.ElementAt(i);
+                    var next_5 = lCheck.ElementAt(i + 1);
+                    var next_6 = lCheck.ElementAt(i + 2);
+                    var next_7 = lCheck.ElementAt(i + 3);
+
+                    if (cur.Open > cur.Close //Nến đỏ
+                        || cur.Close <= prev_1.Close
+                        || cur.Close <= prev_2.Close
+                        || cur.Close <= prev_3.Close
+                        || cur.Close <= prev_4.Close
+                        || cur.Close < next_5.Close
+                        || cur.Close < next_6.Close
+                        || cur.Close < next_7.Close)
+                        continue;
+
+                    //check BB
+                    var bb = lbb.First(x => x.Date == cur.Date);
+                    if (cur.High > (decimal)bb.UpperBand.Value)
+                        continue;
+
+                    return true;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return false;
+        }
+
+        public static bool IsExistBotB(this List<Quote> lData)
+        {
+            try
+            {
+                var lbb = lData.GetBollingerBands();
+                var lCheck = lData.TakeLast(20);
+                var count = lCheck.Count();
+                for (int i = 4; i < count - 3; i++)
+                {
+                    var prev_1 = lCheck.ElementAt(i - 4);
+                    var prev_2 = lCheck.ElementAt(i - 3);
+                    var prev_3 = lCheck.ElementAt(i - 2);
+                    var prev_4 = lCheck.ElementAt(i - 1);
+                    var cur = lCheck.ElementAt(i);
+                    var next_5 = lCheck.ElementAt(i + 1);
+                    var next_6 = lCheck.ElementAt(i + 2);
+                    var next_7 = lCheck.ElementAt(i + 3);
+
+                    if (cur.Open < cur.Close //Nến đỏ
+                        || cur.Close >= prev_1.Close
+                        || cur.Close >= prev_2.Close
+                        || cur.Close >= prev_3.Close
+                        || cur.Close >= prev_4.Close
+                        || cur.Close > next_5.Close
+                        || cur.Close > next_6.Close
+                        || cur.Close > next_7.Close)
+                        continue;
+
+                    //check BB
+                    var bb = lbb.First(x => x.Date == cur.Date);
+                    if (cur.Low < (decimal)bb.LowerBand.Value)
+                        continue;
+
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return false;
         }
     }
 }
