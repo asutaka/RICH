1c06cd9b0d25dfd8b05d5e34446d69fbf1427bab
Nguyễn Văn Phú <phunv@ftech.ai>
2025-06-18 17:02:06 +0700

    zz

--- TestPr/Service/TestService.cs
+++ TestPr/Service/TestService.cs
@@ -719,7 +719,7 @@ namespace TestPr.Service
                 var dt = DateTime.UtcNow;
                 var lAll = await StaticVal.ByBitInstance().V5Api.ExchangeData.GetLinearInverseSymbolsAsync(Category.Linear, limit: 1000);
                 var lUsdt = lAll.Data.List.Where(x => x.QuoteAsset == "USDT" && !x.Name.StartsWith("1000")).Select(x => x.Name);
-                var lTake = lUsdt.Skip(0).Take(100);
+                var lTake = lUsdt.Skip(200).Take(100);
                 var lRank = new List<clsShow>();
 
                 foreach (var s in lTake)
