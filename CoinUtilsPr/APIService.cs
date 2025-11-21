ef3fc6db077223224ac915bdfa5dba79d04bd92f
Nguyễn Văn Phú <phunv@ftech.ai>
2025-11-21 17:24:12 +0700

    zzz

--- CoinUtilsPr/APIService.cs
+++ CoinUtilsPr/APIService.cs
@@ -11,7 +11,8 @@ namespace CoinUtilsPr
     public interface IAPIService
     {
         Task<List<Quote>> GetData_Bybit(string symbol, int DAY = 10, int SKIP_DAY = 0);
-        Task<List<Quote>> GetData_Binance(string symbol, EInterval interval, long fromTime = 0);
+        Task<List<Quote>> GetData_Binance(string symbol, EInterval interval);
+        Task<List<Quote>> GetData_Binance(string symbol, EInterval interval, int DAY = 10, int SKIP_DAY = 0);
         Task<List<Quote>> GetData_Bybit_1H(string symbol);
         Task<CoinAnk_LiquidValue> CoinAnk_GetLiquidValue(string coin);
     }
@@ -149,14 +150,94 @@ namespace CoinUtilsPr
             return lAll;
         }
 
-        public async Task<List<Quote>> GetData_Binance(string symbol, EInterval interval, long fromTime = 0)
+        public async Task<List<Quote>> GetData_Binance(string symbol, EInterval interval, int DAY = 10, int SKIP_DAY = 0)
+        {
+            var lAll = new List<Quote>();
+            try
+            {
+                var now = DateTimeOffset.UtcNow;
+                var from = now.AddDays(-DAY);
+                var to = now.AddDays(-SKIP_DAY);
+                var divDay = (int)(to - from).TotalDays;
+                var songay = 10; //Ứng với khung 15m
+                if (interval == EInterval.H1)
+                {
+                    songay = 40;
+                }
+                var even = Math.Ceiling((double)divDay / songay);
+                for (int i = 0; i < even; i++)
+                {
+                    var fromTime = from.AddDays(i * 10);
+                    var toTime = fromTime.AddDays(10);
+                    var lres = await GetData_Bybit(symbol, fromTime, toTime);
+                    Thread.Sleep(200);
+                    if (lres.Any())
+                    {
+                        lAll.AddRange(lres);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"APIService.GetData_Binance|EXCEPTION|INPUT: symbol:{symbol}| {ex.Message}");
+            }
+            return lAll;
+        }
+
+        public async Task<List<Quote>> GetData_Binance(string symbol, EInterval interval, DateTimeOffset from, DateTimeOffset to)
+        {
+            var url = string.Format("https://www.binance.com/fapi/v1/continuousKlines?startTime={2}&endTime={3}&limit=1000&pair={0}&contractType=PERPETUAL&interval={1}", symbol, interval.GetDisplayName(), from.ToUnixTimeSeconds(), to.ToUnixTimeSeconds());
+
+            try
+            {
+                using var client = _client.CreateClient();
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders
+                      .Accept
+                      .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "");
+                request.Content = new StringContent("",
+                                                    Encoding.UTF8,
+                                                    "application/json");
+
+                var response = await client.SendAsync(request);
+                var contents = await response.Content.ReadAsStringAsync();
+                if (contents.Length < 200)
+                    return new List<Quote>();
+
+                var lArray = JArray.Parse(contents);
+                if (lArray.Any())
+                {
+                    var lOut = lArray.Select(x => new Quote
+                    {
+                        Date = long.Parse(x[0].ToString()).UnixTimeStampMinisecondToDateTime(),
+                        Open = decimal.Parse(x[1].ToString()),
+                        High = decimal.Parse(x[2].ToString()),
+                        Low = decimal.Parse(x[3].ToString()),
+                        Close = decimal.Parse(x[4].ToString()),
+                        Volume = decimal.Parse(x[5].ToString()),
+                    }).ToList();
+                    return lOut;
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"APIService.GetData_Binance|EXCEPTION|INPUT: symbol:{symbol}| {ex.Message}");
+            }
+            return new List<Quote>();
+        }
+
+        public async Task<List<Quote>> GetData_Binance(string symbol, EInterval interval)
         {
             //
-            var url = string.Format("https://www.binance.com/fapi/v1/continuousKlines?startTime={2}&limit=1000&pair={0}&contractType=PERPETUAL&interval={1}", symbol, interval.GetDisplayName(), fromTime);
-            if (fromTime <= 0)
-            {
-                url = string.Format("https://www.binance.com/fapi/v1/continuousKlines?limit=1000&pair={0}&contractType=PERPETUAL&interval={1}", symbol, interval.GetDisplayName()); ;
-            }
+            //var url = string.Format("https://www.binance.com/fapi/v1/continuousKlines?startTime={2}&limit=1000&pair={0}&contractType=PERPETUAL&interval={1}", symbol, interval.GetDisplayName(), fromTime);
+            //if (fromTime <= 0)
+            //{
+
+            //}
+
+            var url = string.Format("https://www.binance.com/fapi/v1/continuousKlines?limit=1000&pair={0}&contractType=PERPETUAL&interval={1}", symbol, interval.GetDisplayName());
 
             try
             {
